f0e3734663ccae68da51ef0df45d0251782daf30
NimnaKs <nimnakaveesha123456@gmail.com>
2024-02-03 11:16:55 +0530

    Add Q1 using path info and ant pattern

--- src/main/java/lk/ijse/practise/api/Customer.java
+++ src/main/java/lk/ijse/practise/api/Customer.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "customer", urlPatterns = "/customer")
+@WebServlet(name = "customer", urlPatterns = "/customer/*")
 public class Customer extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -17,5 +17,11 @@ public class Customer extends HttpServlet {
         System.out.println("Hello Mr. "+firstName+" "+lastName+"!");
         var age = req.getParameter("age");
         System.out.println("Your age is : "+age);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name= req.getPathInfo();
+        System.out.println("Hello"+name);
     }
 }
